1ed9108ba0c83b53f99059d5b5bb382215e71470
Modestas <modestas.kazlauskas@ktu.edu>
2021-02-08 19:59:27 +0200

    Initial Commit

---
+++ TowerDefence/Assets/Scripts/Enemy_Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Script : MonoBehaviour
+{
+    public float speed = 10f;
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
